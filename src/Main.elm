6c78ecb78a22b45efb8d3225218917a9c74970d5
Jordan Cotter <jrdn.cttr@gmail.com>
2019-12-08 17:22:22 -0600

    text on days

--- src/Main.elm
+++ src/Main.elm
@@ -36,14 +36,43 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-      [ calender "December" 31 0 [11, 12, 15, 27, 28, 29, 30, 31]
-      , calender "January" 31 3 [1]
+      [ calender "December" 31 0 [{day=11, text="horse racing"}, {day=12, text="horse racing"}, {day=15, text="horse racing"}, {day=27, text="casey out of town"}, {day=28, text="casey out of town"}, {day=29, text="casey out of town"}, {day=30, text="casey out of town"}, {day=31, text="casey out of town"}]
+      , calender "January" 31 3 [{day=1, text="casey out of town"}]
       , calender "February" 30 6 []
       ]
 
 
 calender monthName daysInMonth dayMonthStarts hangOutDays =
   month monthName (daysInMonth+dayMonthStarts) (days dayMonthStarts hangOutDays)
+
+month name numOfDays monthDays = div [] [
+        Html.h2 [] [text name]
+      , weekDays
+      , div [ style "display" "grid"
+            , style "grid-template-columns" "1fr 1fr 1fr 1fr 1fr 1fr 1fr "
+            ]
+            (List.range 1 numOfDays |> List.indexedMap monthDays)
+      ]
+
+days offset dtpDays i calenderDayFromRange = div
+          [ style "width" "100px"
+          , style "height" "100px"
+          , style "border" "1px solid black"
+          , style "background-color" (bgColor offset dtpDays i)
+          ]
+          [ String.fromInt (if i < offset then 0 else i-(offset-1)) |> text
+          , text (getText offset dtpDays i)
+          ]
+getText offset dtpDays i =
+  if (List.any (match offset i) dtpDays)
+  then (List.filter (match offset i) dtpDays) |> (List.head) |> Maybe.withDefault {day=i, text=""} |> .text
+  else ""
+
+match offset i x = x.day == (i-(offset-1))
+bgColor offset openDays index =
+  if (List.any (match offset index) openDays)
+  then "gray"
+  else "white"
 
 weekDays = div [ style "display" "grid"
             , style "grid-template-columns" "1fr 1fr 1fr 1fr 1fr 1fr 1fr "
@@ -56,24 +85,3 @@ weekDays = div [ style "display" "grid"
             , span [] [text "Friday"]
             , span [] [text "Saturday"]
         ]
-days offset dtpDay i x = div
-          [ style "width" "100px"
-          , style "height" "100px"
-          , style "border" "1px solid black"
-          , style "background-color" (bgColor offset dtpDay i)
-          ] [String.fromInt (if i < offset then 0 else i-(offset-1)) |> text]
-
-match offset i x = x == (i-(offset-1))
-bgColor offset openDays index =
-  if (List.any (match offset index) openDays)
-  then "gray"
-  else "white"
-
-month name numOfDays monthDays = div [] [
-        Html.h2 [] [text name]
-      , weekDays
-      , div [ style "display" "grid"
-            , style "grid-template-columns" "1fr 1fr 1fr 1fr 1fr 1fr 1fr "
-            ]
-            (List.range 1 numOfDays |> List.indexedMap monthDays)
-      ]
